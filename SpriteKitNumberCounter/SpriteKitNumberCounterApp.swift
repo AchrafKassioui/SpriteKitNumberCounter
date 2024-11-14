8f7a9e14407d396b25a22586de3455af93582268
Achraf Kassioui <achrafkassioui@gmail.com>
2024-11-14 19:38:18 +0100

    Initial Commit

---
+++ SpriteKitNumberCounter/SpriteKitNumberCounterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKitNumberCounterApp.swift
+//  SpriteKitNumberCounter
+//
+//  Created by Achraf Kassioui on 14/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKitNumberCounterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
